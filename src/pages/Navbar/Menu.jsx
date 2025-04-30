496ba93365d3555e7453ba9e7391ab0bb67af9fc
Jenni <109981562+jennial@users.noreply.github.com>
2025-04-29 21:41:13 -0300

    ajuste menu

--- src/pages/Navbar/Menu.jsx
+++ src/pages/Navbar/Menu.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Menu.css'; 
+import './menu.css'; 
 
 function Menu() {
   const [isOpen, setIsOpen] = useState(false);
